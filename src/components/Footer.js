aa86cf53b4368385b3cae9d9279067f507fc5210
Jaimal Dullat <jaimaldullat@gmail.com>
2019-04-05 19:11:55 -0400

    Added Main Files

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            <p className='todo-footer'>&copy; 2019, Jaimal Dullat</p>
+        </footer>
+    )
+};
+export default Footer;
