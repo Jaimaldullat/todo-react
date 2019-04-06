69194147e9b50110181003a89a02dcf5b3ceed42
Jaimal Dullat <jaimaldullat@gmail.com>
2019-04-06 17:51:39 -0400

    Everything Done!

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -12,6 +12,7 @@ class MainContent extends React.Component {
         this.onAddItem = this.onAddItem.bind(this);
         this.onChangeValue = this.onChangeValue.bind(this);
         this.uuidv4 = this.uuidv4.bind(this);
+        this.handleKeyPress = this.handleKeyPress.bind(this);
     }
 
     handleChange(id) {
@@ -31,13 +32,13 @@ class MainContent extends React.Component {
     onAddItem() {
         if (this.state.value) {
             this.setState(preState => {
-                const todos = preState.todos.concat(
+                const todos = [
                     {
-                        value:preState.value,
+                        value: preState.value,
                         id: this.uuidv4(),
                         completed: false
-                    }
-                    );
+                    }, ...preState.todos
+                ];
                 return {
                     todos,
                     value: ''
@@ -45,42 +46,50 @@ class MainContent extends React.Component {
             })
         }
     }
+
     onChangeValue(e) {
-            this.setState({
-                    value: e.target.value
-            })
+        this.setState({
+            value: e.target.value
+        })
     }
-     uuidv4() {
-        return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+
+    handleKeyPress(e) {
+        if (e.key == 'Enter') {
+            this.onAddItem();
+        }
+    }
+
+    uuidv4() {
+        return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
             (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
         )
     }
 
-render()
-{
-   const todoItems =  this.state.todos.length > 0 ? this.state.todos.map(item => <TodoItem key={item.id} handleChange={this.handleChange}
-                                                             item={item} />): <li style={{border:'none'}}>No item added</li>;
-    return (
-        <main>
-            <div className='todo-main-div'>
-                <ul>
-                    {//this.state.todos
-                         }
-                    <label>
-                        <input
-                            name='name'
-                            value={this.state.value}
-                            onChange={this.onChangeValue}
-                            autoComplete='off'
-                            type='text'/>
-                        <button onClick={this.onAddItem}>Add</button>
-                    </label>
-                    {todoItems}
-                </ul>
-            </div>
-        </main>
-    )
-}
+    render() {
+        const todoItems = this.state.todos.length > 0 ?
+            this.state.todos.map(item =>
+                <TodoItem key={item.id} handleChange={this.handleChange} item={item}/>) :
+            <li style={{border: 'none'}}>No item added</li>;
+        return (
+            <main>
+                <div className='todo-main-div'>
+                    <ul>
+                        <label>
+                            <input
+                                name='name'
+                                value={this.state.value}
+                                onChange={this.onChangeValue}
+                                onKeyPress={this.handleKeyPress}
+                                autoComplete='off'
+                                type='text'/>
+                            <button onClick={this.onAddItem}>Add</button>
+                        </label>
+                        {todoItems}
+                    </ul>
+                </div>
+            </main>
+        )
+    }
 }
 ;
 export default MainContent;
